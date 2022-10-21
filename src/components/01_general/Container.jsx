e79001a65461686ca9255c5e1c2c8f3f8e95350e
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-21 17:13:12 +0200

    styled (flex and gap)

--- src/components/01_general/Container.jsx
+++ src/components/01_general/Container.jsx
@@ -4,6 +4,9 @@ import styled from "styled-components";
 const ContainerElement = styled.div`
   min-width: 327px;
   max-width: 730px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `
 
 export function Container({children}) {
