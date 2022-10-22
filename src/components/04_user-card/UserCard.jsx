31f32db1d1b875f27cfbbcff4d356ae3561106f9
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-22 12:03:36 +0200

    basic component structure

--- src/components/04_user-card/UserCard.jsx
+++ src/components/04_user-card/UserCard.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+import {colorPalette} from "../../general/colorPallete";
+import {Statistics} from "./Statistics";
+import {Links} from "./Links";
+import {BasicInfo} from "./BasicInfo";
+import {Avatar} from "./Avatar";
+
+
+
+const Card = styled.section`
+  background-color: ${colorPalette.dark.dark};
+  width: 100%;
+  min-height: 250px;
+  border-radius: 15px;
+  padding: 44px 48px;
+  display: flex;
+  gap: 3rem;
+`
+
+const Div = styled.div`
+    display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`
+
 
 export function UserCard() {
     return (
-        <div>UserCard</div>
-
+        <Card>
+            <Avatar/>
+            <Div>
+                <BasicInfo />
+                <Statistics/>
+                <Links />
+            </Div>
+        </Card>
     )
 }
