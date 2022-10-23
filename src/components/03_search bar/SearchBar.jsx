c7de0b74678c775a33ef019c9eac6845cb6bad6f
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-23 12:41:47 +0200

    added label

--- src/components/03_search bar/SearchBar.jsx
+++ src/components/03_search bar/SearchBar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import search from "../../img/icon-search.svg"
+import search from "../../img/icon-search.svg";
 import {Bar, Search, Icon, Input, Button} from "./SearchBar.style";
 
 export function SearchBar({onChange, onKeyUp, value, onClick}) {
     return (
         <Bar>
             <Search>
-                <Icon src={search} alt="search"></Icon>
+                <label htmlFor="search">
+                    <Icon src={search} alt="search"></Icon>
+                </label>
                 <Input placeholder="Search GitHub username…"
+                       id="search"
                        onChange={onChange}
                        onKeyUp={onKeyUp}
                        value={value}
                 ></Input>
             </Search>
-            <Button onClick={onClick}>Search</Button>
+            <Button
+                onClick={onClick}
+            >
+                Search
+            </Button>
         </Bar>
     )
 }
