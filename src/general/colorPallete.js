633e6fb7745833b91fb41fb45808009cf73528bb
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-23 13:02:04 +0200

    added accentLight color

--- src/general/colorPallete.js
+++ src/general/colorPallete.js
@@ -3,6 +3,7 @@ export const colorPalette = {
         veryDark: 'hsl(220, 40%, 13%)',
         dark: 'hsl(222, 41%, 20%)',
         accent: 'hsl(212, 100%, 50%)',
+        accentLight: 'hsl(212, 100%, 60%)',
         light: 'hsl(217, 20%, 51%)',
         white: 'hsl(0, 0%, 100%)'
     }
