4acf0856c0c06b8e6709cd713d589f2563cfb675
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-23 10:04:18 +0200

    basic input functions

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import {Container} from "./components/01_general/Container";
 import {Header} from "./components/02_header/Header";
 import {SearchBar} from "./components/03_search bar/SearchBar";
 import {UserCard} from "./components/04_user-card/UserCard";
+import {useState} from "react";
 
 function App() {
+    const [input, setInput] = useState("");
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter') {
+            console.log('enter')
+        }
+    }
+
     return (
         <Main>
             <Container>
                 <Header/>
-                <SearchBar/>
+                <SearchBar
+                    onChange={handleChange}
+                    onKeyUp={handleEnter}
+                    value={input}/>
                 <UserCard/>
             </Container>
         </Main>
